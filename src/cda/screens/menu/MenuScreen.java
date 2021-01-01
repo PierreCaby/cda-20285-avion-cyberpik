d0e0647b832d89ce54e1bdca4012b487713b6d06
valentin-cochin <cochin.valentin@hotmail.fr>
2021-01-01 23:00:09 +0100

    Add music to the Menu. Move play method from MusicAsset to MenuScreen

--- src/cda/screens/menu/MenuScreen.java
+++ src/cda/screens/menu/MenuScreen.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import cda.commons.Global;
+import cda.commons.libs.MusicAsset;
 import cda.commons.libs.VisualAsset;
 import cda.screens.AbstractScreen;
 import javafx.animation.ScaleTransition;
@@ -44,6 +45,7 @@ public class MenuScreen extends AbstractScreen {
     private Parent createContent() {
         addBackground();
         addTitle();
+        playMusic(MusicAsset.STARTER_MENU);
 
         double lineX = Global.SCREEN_WIDTH / 2 - 100;
         double lineY = Global.SCREEN_HEIGHT / 3 + 50;
