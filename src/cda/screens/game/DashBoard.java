a9febf088de6d42761f3ed4dbea073aef61d316a
valentin-cochin <cochin.valentin@hotmail.fr>
2021-01-04 15:29:26 +0100

    The status bar is displayed on the gameplay screen, it doesn't update and sprites are in front of it

--- src/cda/screens/game/DashBoard.java
+++ src/cda/screens/game/DashBoard.java
@@ -1,6 +1,12 @@
 package cda.screens.game;
 
 import cda.actors.friendly.Ship;
+import cda.commons.Global;
+import cda.commons.libs.FontAsset;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 /**
  * classe responsable des affichages "tableau" de bord : le score et la status
@@ -10,7 +16,8 @@ import cda.actors.friendly.Ship;
  */
 
 public class DashBoard {
-	private static final String MSG_STATUS_BAR = "SchmupGame // Score : %d // lifeCount : %d // Shield : %02.2f %% // Weapon : %02.2f %%";
+	private static final String MSG_STATUS_BAR = "SchmupGame // Score : %d // lifeCount : %d //\n"
+			+ "   Shield : %02.2f %% // Weapon : %02.2f %%";
 	private static final int PADDING = 10;
 	private int hauteurBarre;
 
@@ -19,9 +26,10 @@ public class DashBoard {
 	public DashBoard(GamePlayScreen gamePlayScreen) {
 		this.gamePlayScreen = gamePlayScreen;
 		this.addStatusBar();
-		
+
 		// this.footer = TextureAsset.FOOTER.get();
-		// this.hauteurBarre = (Global.SCREEN_HEIGHT - footer.getHeight() - (PADDING * 2)
+		// this.hauteurBarre = (Global.SCREEN_HEIGHT - footer.getHeight() - (PADDING *
+		// 2)
 		// - 60);
 	}
 
@@ -36,8 +44,20 @@ public class DashBoard {
 
 	private void addStatusBar() {
 		Ship ship = gamePlayScreen.getShip();
-		// TO DO ajouter notion shield
-		String titleBar = String.format(MSG_STATUS_BAR, ship.getPoint(), ship.getLifeCount(), 95.55f, 50f);
-		System.out.println(titleBar);
+		// TO DO ajouter notion shield et arme
+		String textBar = String.format(MSG_STATUS_BAR, ship.getPoint(), ship.getLifeCount(), 95.55f, 50f);
+		System.out.println(textBar);
+
+		Text text = new Text(textBar);
+
+		text.setFont(Font.loadFont(FontAsset.FIVE_BY_FIVE.getFilePath(), 16));
+		text.setFill(Color.WHITE);
+
+		Pane pane = new Pane();
+		pane.setTranslateX(Global.SCREEN_WIDTH / 2 - text.getLayoutBounds().getWidth() / 2);
+		pane.setTranslateY(Global.SCREEN_HEIGHT / 20 - text.getLayoutBounds().getHeight() / 20);
+
+		pane.getChildren().addAll(text);
+		gamePlayScreen.getRoot().getChildren().add(pane);
 	}
 }
