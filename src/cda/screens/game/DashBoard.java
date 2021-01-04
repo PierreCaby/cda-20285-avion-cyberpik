c08f3fc286931926142e68e908f106abcef69a9e
valentin-cochin <cochin.valentin@hotmail.fr>
2021-01-04 07:12:38 +0100

    Add private methods in DashBoard

--- src/cda/screens/game/DashBoard.java
+++ src/cda/screens/game/DashBoard.java
@@ -1,5 +1,7 @@
 package cda.screens.game;
 
+import cda.actors.friendly.Ship;
+
 /**
  * classe responsable des affichages "tableau" de bord : le score et la status
  * bar.
@@ -7,23 +9,35 @@ package cda.screens.game;
  *
  */
 
-public class DashBoard
-{
+public class DashBoard {
 	private static final String FMT_MSG_BAR = "SchmupGame // FPS : %d // lifePoints : %d // Shield : %02.2f %% // lifeCount : %d";
 	private static final int PADDING = 10;
 	private static final int MARGE = 10;
 	private int hauteurBarre;
 
-	// private Color weaponColor = new Color(0f, 1f, 0f, 0.5f);
 	private GamePlayScreen gamePlayScreen;
-	// private Texture footer;
-	// private BitmapFont font = TrueTypeFont.SHARETECH_12.getFont();
 
-	public DashBoard(GamePlayScreen gamePlayScreen)
-	{
+	public DashBoard(GamePlayScreen gamePlayScreen) {
 		super();
 		this.gamePlayScreen = gamePlayScreen;
 		// this.footer = TextureAsset.FOOTER.get();
-		// this.hauteurBarre = (Global.SCREEN_HEIGHT - footer.getHeight() - (MARGE * 2) - 60);
+		// this.hauteurBarre = (Global.SCREEN_HEIGHT - footer.getHeight() - (MARGE * 2)
+		// - 60);
+	}
+	
+	private void addLifeCount() {
+	}
+	
+	private void addShieldBar() {
+	}
+	
+	private void addScore() {	
+	}
+	
+	private void addStatusBar() {
+		int fps = 60; // TO DO get the speed somewhere
+		Ship ship = gamePlayScreen.getShip();
+		// TO DO ajouter notion shield
+		String titleBar = String.format(FMT_MSG_BAR, fps, ship.getLifeCount(), "Ajouter shield", ship.getLifeCount());
 	}
 }
